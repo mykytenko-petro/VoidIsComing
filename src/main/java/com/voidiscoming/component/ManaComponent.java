062cfa833b4368f44f2e2f72fbb2cd822d1fd22f
Mykytenko Petro <mikitenkot8@gmail.com>
2026-07-23 18:34:20 +0300

    WIP mana system

---
+++ src/main/java/com/voidiscoming/component/ManaComponent.java
@@ -0,0 +1,12 @@
+package com.voidiscoming.component;
+
+import dev.onyxstudios.cca.api.v3.component.Component;
+
+public interface ManaComponent extends Component {
+    int getMana();
+    int getMaxMana();
+    
+    void setMana(int mana);
+    void addMana(int amount);
+    void removeMana(int amount);
+}
