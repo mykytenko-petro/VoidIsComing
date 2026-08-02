167ddbeacb115f2514445df73820f4644722d30e
andrewskulskuia <andrewskulskyia@gmail.com>
2026-08-02 19:48:45 +0300

    add stone golem

--- src/main/java/com/voidiscoming/client/Model/StoneGolemModel.java
+++ src/main/java/com/voidiscoming/client/Model/StoneGolemModel.java
@@ -1,13 +1,18 @@
 package com.voidiscoming.client.Model;
 
-import net.minecraft.client.model.*;
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
-import net.minecraft.client.util.math.MatrixStack;
 
 public class StoneGolemModel<T extends Entity> extends EntityModel<T> {
     public static final EntityModelLayer LAYER_LOCATION = new EntityModelLayer(new Identifier("voidiscoming", "stone_golem"), "main");
