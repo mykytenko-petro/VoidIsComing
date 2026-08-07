586108e2b725f3f428b6c9edaa6e7b6f691430cb
Mykytenko Petro <mikitenkot8@gmail.com>
2026-08-07 12:36:51 +0300

    refactor: reorganize HandledScreenAccessor and update mixin package structure

--- src/main/java/com/voidiscoming/client/mixin/gui/HandledScreenAccessor.java
+++ src/main/java/com/voidiscoming/client/mixin/gui/HandledScreenAccessor.java
@@ -1,4 +1,4 @@
-package com.voidiscoming.common.mixin.gui;
+package com.voidiscoming.client.mixin.gui;
 
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import org.spongepowered.asm.mixin.Mixin;
