e0f4335794ca138adb7b053c9c8b94272aecda87
Mykytenko Petro <mikitenkot8@gmail.com>
2026-08-12 12:07:39 +0300

    Add new skill nodes for Warrior, Archer, and Mage classes; update heal spell identifier and add corresponding textures

--- src/main/java/com/voidiscoming/common/mechanic/skill/ModSkills.java
+++ src/main/java/com/voidiscoming/common/mechanic/skill/ModSkills.java
@@ -12,10 +12,18 @@ import net.minecraft.util.Identifier;
 public class ModSkills {
     private static final Map<Identifier, SkillNode> SKILLS = new HashMap<>();
 
-    public static final Identifier HEAL_SPELL = VoidIsComing.id("heal_spell");
+    public static final Identifier HEAL_SPELL = VoidIsComing.id("heal_spell_skill");
+    
+    public static final Identifier WARRIOR_CLASS = VoidIsComing.id("warrior_class_skill");
+    public static final Identifier ARCHER_CLASS = VoidIsComing.id("archer_class_skill");
+    public static final Identifier MAGE_CLASS = VoidIsComing.id("mage_class_skill");
 
     public static void registerSkills() {
         registerSkill(new SkillNode(HEAL_SPELL, Optional.empty(), 1, SkillType.SPELL));
+
+        registerSkill(new SkillNode(WARRIOR_CLASS, Optional.of(HEAL_SPELL), 4, SkillType.CLASS));
+        registerSkill(new SkillNode(ARCHER_CLASS, Optional.of(HEAL_SPELL), 4, SkillType.CLASS));
+        registerSkill(new SkillNode(MAGE_CLASS, Optional.of(HEAL_SPELL), 4, SkillType.CLASS));
     }
 
     private static void registerSkill(SkillNode node) {
