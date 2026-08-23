7fb43717071ca1717994cc3d7c0d613d98f94bce
Mykytenko Petro <mikitenkot8@gmail.com>
2026-08-23 18:32:47 +0300

    feat: Add new skills and bonuses for warrior, archer, and mage classes; update skill descriptions and textures

--- src/main/java/com/voidiscoming/common/mechanic/skill/ModSkills.java
+++ src/main/java/com/voidiscoming/common/mechanic/skill/ModSkills.java
@@ -19,6 +19,9 @@ public class ModSkills {
     // warrior
     public static final Identifier WARRIOR_CLASS = VoidIsComing.id("warrior_class_skill");
     public static final Identifier ARMOR_BONUS = VoidIsComing.id("armor_bonus_skill");
+    public static final Identifier ARMOR_BONUS_2 = VoidIsComing.id("armor_bonus_2_skill");
+    public static final Identifier STONE_BASTION = VoidIsComing.id("stone_bastion_skill");
+    public static final Identifier RESILIENCE = VoidIsComing.id("resilience_skill");
     public static final Identifier SWORD_POWER = VoidIsComing.id("sword_power_skill");
     public static final Identifier RAGE_SPELL = VoidIsComing.id("rage_spell_skill");
     public static final Identifier VAMPIRISM_SPELL = VoidIsComing.id("vampirism_spell_skill");
@@ -26,6 +29,9 @@ public class ModSkills {
     // archer
     public static final Identifier ARCHER_CLASS = VoidIsComing.id("archer_class_skill");
     public static final Identifier HEALTH_BONUS = VoidIsComing.id("health_bonus_skill");
+    public static final Identifier HEALTH_BONUS_2 = VoidIsComing.id("health_bonus_2_skill");
+    public static final Identifier WILD_BLOOM = VoidIsComing.id("wild_bloom_skill");
+    public static final Identifier SWIFTSTEP = VoidIsComing.id("swiftstep_skill");
     public static final Identifier BOW_POWER = VoidIsComing.id("bow_power_skill");
     public static final Identifier HUNTER_SENSE_SPELL = VoidIsComing.id("hunter_sense_spell_skill");
     public static final Identifier INVISIBILITY_SPELL = VoidIsComing.id("invisibility_spell_skill");
@@ -33,6 +39,9 @@ public class ModSkills {
     // mage
     public static final Identifier MAGE_CLASS = VoidIsComing.id("mage_class_skill");
     public static final Identifier MANA_BONUS = VoidIsComing.id("mana_bonus_skill");
+    public static final Identifier MANA_BONUS_2 = VoidIsComing.id("mana_bonus_2_skill");
+    public static final Identifier ABYSSAL_RESERVOIR = VoidIsComing.id("abyssal_reservoir_skill");
+    public static final Identifier AFFINITY = VoidIsComing.id("affinity_skill");
     public static final Identifier WAND_POWER = VoidIsComing.id("wand_power_skill");
     public static final Identifier FROST_AURA_SPELL = VoidIsComing.id("frost_aura_spell_skill");
     public static final Identifier TELEPORTATION_SPELL = VoidIsComing.id("teleportation_spell_skill");
@@ -41,14 +50,29 @@ public class ModSkills {
         registerSkill(new SkillNode(HEAL_SPELL, 1, ModSpells.HEAL));
 
         registerSkill(new SkillNode(WARRIOR_CLASS, HEAL_SPELL, 4, new Identifier[] {ARCHER_CLASS, MAGE_CLASS}));
-        registerSkill(new SkillNode(RAGE_SPELL,WARRIOR_CLASS,3, ModSpells.RAGE));
-        registerSkill(new SkillNode(VAMPIRISM_SPELL,WARRIOR_CLASS,3, ModSpells.VAMPIRISM));
+        registerSkill(new SkillNode(ARMOR_BONUS, WARRIOR_CLASS, 1));
+        registerSkill(new SkillNode(ARMOR_BONUS_2, ARMOR_BONUS, 1));
+        registerSkill(new SkillNode(STONE_BASTION, ARMOR_BONUS_2, 1));
+        registerSkill(new SkillNode(RESILIENCE, STONE_BASTION, 1));
+        registerSkill(new SkillNode(SWORD_POWER, WARRIOR_CLASS, 1));
+        registerSkill(new SkillNode(RAGE_SPELL, WARRIOR_CLASS, 3, ModSpells.RAGE));
+        registerSkill(new SkillNode(VAMPIRISM_SPELL, WARRIOR_CLASS, 3, ModSpells.VAMPIRISM));
 
         registerSkill(new SkillNode(ARCHER_CLASS, HEAL_SPELL, 4, new Identifier[] {WARRIOR_CLASS, MAGE_CLASS}));
-        registerSkill(new SkillNode(HUNTER_SENSE_SPELL,ARCHER_CLASS,3, ModSpells.HUNTER_SENSE));
-        registerSkill(new SkillNode(INVISIBILITY_SPELL,ARCHER_CLASS,3, ModSpells.INVISIBILITY));
+        registerSkill(new SkillNode(HEALTH_BONUS, ARCHER_CLASS, 1));
+        registerSkill(new SkillNode(HEALTH_BONUS_2, HEALTH_BONUS, 1));
+        registerSkill(new SkillNode(WILD_BLOOM, HEALTH_BONUS_2, 1));
+        registerSkill(new SkillNode(SWIFTSTEP, WILD_BLOOM, 1));
+        registerSkill(new SkillNode(BOW_POWER, ARCHER_CLASS, 1));
+        registerSkill(new SkillNode(HUNTER_SENSE_SPELL, ARCHER_CLASS, 3, ModSpells.HUNTER_SENSE));
+        registerSkill(new SkillNode(INVISIBILITY_SPELL, ARCHER_CLASS, 3, ModSpells.INVISIBILITY));
 
         registerSkill(new SkillNode(MAGE_CLASS, HEAL_SPELL, 4, new Identifier[] {ARCHER_CLASS, WARRIOR_CLASS}));
+        registerSkill(new SkillNode(MANA_BONUS, MAGE_CLASS, 1));
+        registerSkill(new SkillNode(MANA_BONUS_2, MANA_BONUS, 1));
+        registerSkill(new SkillNode(ABYSSAL_RESERVOIR, MANA_BONUS_2, 1));
+        registerSkill(new SkillNode(AFFINITY, ABYSSAL_RESERVOIR, 1));
+        registerSkill(new SkillNode(WAND_POWER, MAGE_CLASS, 1));
         registerSkill(new SkillNode(FROST_AURA_SPELL, MAGE_CLASS, 3, ModSpells.FROST_AURA));
         registerSkill(new SkillNode(TELEPORTATION_SPELL, MAGE_CLASS, 3, ModSpells.TELEPORT));
     }
